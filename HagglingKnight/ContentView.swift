bcad607141eea8692792973d22e28bf1341503c7
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-04-08 13:28:29 +0800

    Initial Commit

---
+++ HagglingKnight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HagglingKnight
+//
+//  Created by Tude Maha on 08/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HagglingKnight/HagglingKnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HagglingKnightApp.swift
+//  HagglingKnight
+//
+//  Created by Tude Maha on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HagglingKnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
